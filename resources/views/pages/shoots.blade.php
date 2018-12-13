bae96882702b056c7f63f142b2973e8348319ea1
Kate Morton <katemortonphoto@icloud.com>
2018-12-13 10:58:28 +0000

    slug url added to link

--- resources/views/pages/shoots.blade.php
+++ resources/views/pages/shoots.blade.php
@@ -37,7 +37,7 @@
                         <img class="card-img rounded-0 image lazy" data-src="/storage/featured_images/{{$post->featured_image}}" alt="{{$post->alt_featured}}">
                         <div class="card-img-overlay text-left">                    
                             <div class="blog-card align-self-baseline">     
-                                <h5 class="card-title mb-0 pb-0"><a href="/posts/{{$post->id}}">{{$post->names}}</a></h5> 
+                                <h5 class="card-title mb-0 pb-0"><a href="{{ url('blog/' . $post->slug) }}">{{$post->names}}</a></h5> 
                                 <h4 class="blog-title mb-1">{{$post->venue}}</h4>
                             </div>                            
                         </div>
